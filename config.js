b7dbdb63878d8aa02c2aa3d023c282f13e0e46fb
ivyblue092 <knezovicivanka092@gmail.com>
2020-07-16 01:43:40 +0200

    shopBackend

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+    MONGODB_URL: process.env.MONGODB_URL || '******************',
+    JWT_SECRET: process.env.JWT_SECRET || '**********************',
+    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb'
+}
